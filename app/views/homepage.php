c730f0b36ee56af0e0bf87f23fb2c16e8c517a42
singesinge <thomasbout662@gmail.com>
2023-09-26 15:08:47 +0200

    Updated homepage view

--- app/views/homepage.php
+++ app/views/homepage.php
@@ -1,3 +1,21 @@
 <?php
-    echo 'Home Page';
+session_start();
+if (!isset($_SESSION['password'])){
+    header ('Location: connection.php');
+    exit;
+}
+$name = isset($_SESSION['name']) ? $_SESSION['name'] : '';
+echo $_SESSION ['name'];
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+    <meta charset="utf-8">
+</head>
+<body>
+    <a href="disconnection.php">
+        <button>Se déconnecter</button>
+    </a>
+</body> 
+</html>

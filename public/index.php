0f235dff6a6ee911724293085357daa7a92fd6a1
Edouard-Pon <91964969+Edouard-Pon@users.noreply.github.com>
2023-09-29 14:03:18 +0200

    Updated router

--- public/index.php
+++ public/index.php
@@ -1,3 +1,20 @@
 <?php
+include '../app/controllers/login.php';
+
+try {
+    if (isset($_SERVER['REQUEST_URI'])) {
+        $route = $_SERVER['REQUEST_URI'];
+
+        switch ($route) {
+            case '/':
+                (new LoginController())->execute();
+                break;
+            default:
+                error_log('404 Not Found');
+        }
+    }
+} catch (Exception) {
+
+}
 
 ?>

77f572c406379823c919c643e1b137f5bd5a3dd9
AyoubBihib22003416 <ayoub.bihi@etu.univ-amu.fr>
2023-09-29 12:02:24 +0200

    Adding of a new page with a navbar

---
+++ app/views/navbar.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Test</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="/public/assets/styles/navbar.css">
+    <link rel="shortcut icon" type="image/jpg" href="/public/assets/images/logoblanc.png"/>
+</head>
+<body class="body">
+<div class="logo"><img src="/public/assets/images/logoblanc.png" class="image" alt="" width="50" height="60"></div>
+<ul>
+
+    <li><a href="Accueil.asp"><img src="/public/assets/images/accueilX.png" width="18" height="18"> Accueil</a></li>
+    <li><a href="Explorer.asp"><img src="/public/assets/images/explorerX.png" width="18" height="18">Explorer</a></li>
+    <li><a href="Notifications.asp"><img src="/public/assets/images/notificationsX.png" width="18" height="18">Notifications</a></li>
+    <li><a href="Messages.asp"><img src="/public/assets/images/messagesX.png" width="18" height="18">Messages</a></li>
+    <li><a href="Listes.asp"><img src="/public/assets/images/listesX.png" width="18" height="18">Listes</a></li>
+    <li><a href="Signets.asp"><img src="/public/assets/images/signetsX.png" width="18" height="18">Signets</a></li>
+    <li><a href="Premium.asp"><img src="/public/assets/images/logonoir.png" width="18" height="18">Blue</a></li>
+    <li><a href="Profil.asp"><img src="/public/assets/images/profilX.png" width="18" height="18">Profil</a></li>
+    <li><a href="Plus.asp"><img src="/public/assets/images/plusX.png" width="18" height="18">Plus</a></li>
+</ul>
+
+
+</html>

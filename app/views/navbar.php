fd49eae5bb1509fd73c8318abaeee89893467241
Louarn Mahis <mathislouarn@gmail.com>
2023-09-30 03:03:22 +0200

    Updated navbar php and css and icons

--- app/views/navbar.php
+++ app/views/navbar.php
@@ -4,25 +4,22 @@ session_start();
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <title>Test</title>
+    <title>navbar</title>
     <meta charset="utf-8">
     <link rel="stylesheet" href="/public/assets/styles/navbar.css">
     <link rel="shortcut icon" type="image/jpg" href="/public/assets/images/logoblanc.png"/>
 </head>
-<body class="body">
-<div class="logo"><img src="/public/assets/images/logoblanc.png" class="image" alt="" width="50" height="60"></div>
-<ul>
+<div>
+<img src="/public/assets/images/logoblanc.png" id="logo" alt="Logo pasX" width="50" height="60">
 
-    <li><a href="Accueil.asp"><img src="/public/assets/images/accueilX.png" width="18" height="18"> Accueil</a></li>
-    <li><a href="Explorer.asp"><img src="/public/assets/images/explorerX.png" width="18" height="18">Explorer</a></li>
-    <li><a href="Notifications.asp"><img src="/public/assets/images/notificationsX.png" width="18" height="18">Notifications</a></li>
-    <li><a href="Messages.asp"><img src="/public/assets/images/messagesX.png" width="18" height="18">Messages</a></li>
-    <li><a href="Listes.asp"><img src="/public/assets/images/listesX.png" width="18" height="18">Listes</a></li>
-    <li><a href="Signets.asp"><img src="/public/assets/images/signetsX.png" width="18" height="18">Signets</a></li>
-    <li><a href="Premium.asp"><img src="/public/assets/images/logonoir.png" width="18" height="18">Blue</a></li>
-    <li><a href="Profil.asp"><img src="/public/assets/images/profilX.png" width="18" height="18">Profil</a></li>
-    <li><a href="Plus.asp"><img src="/public/assets/images/plusX.png" width="18" height="18">Plus</a></li>
-</ul>
-
-
+    <p><a href="Accueil.asp"><img src="/public/assets/images/acceuilX.png" width="18" height="18"> Accueil</a></p>
+    <p><a href="Explorer.asp"><img src="/public/assets/images/explorerX.png" width="18" height="18">Explorer</a></p>
+<!--    <p><a href="Notifications.asp"><img src="/public/assets/images/notificationsX.png" width="18" height="18">Notifications</a></p>-->
+<!--    <p><a href="Messages.asp"><img src="/public/assets/images/messagesX.png" width="18" height="18">Messages</a></p>-->
+<!--    <p><a href="Listes.asp"><img src="/public/assets/images/listesX.png" width="18" height="18">Listes</a></p>-->
+<!--    <p><a href="Signets.asp"><img src="/public/assets/images/signetsX.png" width="18" height="18">Signets</a></p>-->
+    <p><a href="Premium.asp"><img src="/public/assets/images/logoblanc.png" width="18" height="18">Green</a></p>
+    <p><a href="Profil.asp"><img src="/public/assets/images/profilX.png" width="18" height="18">Profil</a></p>
+<!--    <p><a href="Plus.asp"><img src="/public/assets/images/plusX.png" width="18" height="18">Plus</a></p>-->
+</div>
 </html>

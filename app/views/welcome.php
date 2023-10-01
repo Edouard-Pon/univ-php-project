9a1fe8f552142de899fb220f13e2cb5f6817373c
Edouard-Pon <91964969+Edouard-Pon@users.noreply.github.com>
2023-10-01 03:06:03 +0200

    Added redirections for buttons in welcome view

--- app/views/welcome.php
+++ app/views/welcome.php
@@ -11,10 +11,10 @@ class WelcomeView
                 <div class="interface">
                     <p class="bigtext"><b>Nous aussi on <br>peut le faire</b></p>
                     <p class="medtext"><b>Rejoignez dès aujourd'hui</b></p>
-                    <button class="signup"><b>Créer un compte</b></button>
+                    <button class="signup" onclick="window.location.href='/signup'"><b>Créer un compte</b></button>
                     <p class="minitext">En vous inscrivant, vous acceptez les conditions d'utilisation et de confidentialité<br> Politique incluant l'utilisation des cookies.</p>
                     <p class="medtext"><b>Déja un compte?</b></p>
-                    <button class="signin"><b>Se connecter</b></button>
+                    <button class="signin" onclick="window.location.href='/login'"><b>Se connecter</b></button>
                 </div>
             </div>
             <footer>

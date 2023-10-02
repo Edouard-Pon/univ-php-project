321e9fb09274d0009665d3374c3388cfc6f0ab90
YehorKuzm <yehor.kuzmenko@etu.univ-amu.fr>
2023-10-02 09:19:23 +0200

    Updated html and css structure

--- app/views/login.php
+++ app/views/login.php
@@ -5,21 +5,19 @@ class LoginView
     {
         ob_start();
 ?>
-<div class="page-content">
-    <div class="flex-container">
-        <form method="POST" action="">
-            <div class="logo"><img src="assets/images/logoblanc.png" class="image" alt=""></div>
+<div class="flex-container">
+    <form method="POST" action="">
+        <div class="logo"><img src="assets/images/logoblanc.png" class="image" alt=""></div>
 
-            <label for="username">Nom d'utilisateur:</label>
-            <input class="inputText" id="username" type="text" name="name" autocomplete="off" placeholder="Entrez votre nom d'utilisateur">
+        <label for="username">Nom d'utilisateur:</label>
+        <input class="inputText" id="username" type="text" name="name" autocomplete="off" placeholder="Entrez votre nom d'utilisateur">
 
-            <label for="password">Mot de passe:</label>
-            <input class="inputText" type="password" name="password" autocomplete="off" placeholder="Entrez votre mot de passe">
+        <label for="password">Mot de passe:</label>
+        <input class="inputText" type="password" name="password" autocomplete="off" placeholder="Entrez votre mot de passe">
 
-            <a href="/signup">Créer un compte</a>
-            <input type="submit" name="login" id="submitButton">
-        </form>
-    </div>
+        <a href="/signup">Créer un compte</a>
+        <input type="submit" name="login" id="submitButton">
+    </form>
 </div>
 <?php
 include 'partials/footer.php';
--- app/views/welcome.php
+++ app/views/welcome.php
@@ -5,17 +5,15 @@ class WelcomeView
     {
         ob_start();
 ?>
-<div class="page-content">
-    <div class="flex-container">
-        <div class="logo"><img src="assets/images/logoblanc.png" class="image" alt=""></div>
-        <div class="interface">
-            <p class="bigtext"><b>Nous aussi on <br>peut le faire</b></p>
-            <p class="medtext"><b>Rejoignez dès aujourd'hui</b></p>
-            <button class="signup" onclick="window.location.href='/signup'"><b>Créer un compte</b></button>
-            <p class="minitext">En vous inscrivant, vous acceptez les conditions d'utilisation et de confidentialité<br> Politique incluant l'utilisation des cookies.</p>
-            <p class="medtext"><b>Déja un compte?</b></p>
-            <button class="signin" onclick="window.location.href='/login'"><b>Se connecter</b></button>
-        </div>
+<div class="flex-container">
+    <div class="logo"><img src="assets/images/logoblanc.png" class="image" alt=""></div>
+    <div class="interface">
+        <p class="bigtext"><b>Nous aussi on <br>peut le faire</b></p>
+        <p class="medtext"><b>Rejoignez dès aujourd'hui</b></p>
+        <button class="signup" onclick="window.location.href='/signup'"><b>Créer un compte</b></button>
+        <p class="minitext">En vous inscrivant, vous acceptez les conditions d'utilisation et de confidentialité<br> Politique incluant l'utilisation des cookies.</p>
+        <p class="medtext"><b>Déja un compte?</b></p>
+        <button class="signin" onclick="window.location.href='/login'"><b>Se connecter</b></button>
     </div>
 </div>
 <?php

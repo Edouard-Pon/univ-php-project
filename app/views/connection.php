502615682dd37f3a83fc9678d5ff2d5166c93a10
singesinge <thomasbout662@gmail.com>
2023-10-02 11:43:03 +0200

    this is not working

--- app/views/connection.php
+++ app/views/connection.php
@@ -6,9 +6,11 @@ if ($config === false) {
 }
 $bdd = new PDO($config['dsn'], $config['username'], $config['password']);
 
+// Initialiser la variable $message
+$message = "";
+
 // Validation du formulaire
 if (isset($_POST['envoi'])) {
-    $error_message = "";
     if (!empty($_POST['name']) && !empty($_POST['password'])) {
         $name = htmlspecialchars($_POST['name']);
         $password = sha1($_POST['password']);
@@ -21,9 +23,13 @@ if (isset($_POST['envoi'])) {
             $_SESSION['password'] = $password;
             $_SESSION['id'] = $recupUser->fetch()['id'];
 
-            // Mettre à jour la date de dernière connexion
             $actuDate = $bdd->prepare('UPDATE user SET lastco = NOW() WHERE id = ?');
             $actuDate->execute(array($_SESSION['id']));
+
+            // Requête pour récupérer la valeur "admin"
+            $recupAdmin = $bdd->prepare('SELECT admin FROM user WHERE id = ?');
+            $recupAdmin->execute(array($_SESSION['id']));
+
             if ($recupAdmin->rowCount() > 0) {
                 $adminValue = $recupAdmin->fetch()['admin'];
                 
@@ -31,17 +37,15 @@ if (isset($_POST['envoi'])) {
                     header('Location: /app/views/adminpage.php');
                 } elseif ($adminValue == 0) {
                     header('Location: /app/views/homepage.php');
-            }    
-        }
-         else {
+                }
+            }
+        } else {
             $message = "Votre mot de passe ou nom d'utilisateur est incorrect...";
         }
     } else {
         $message = "Veuillez compléter tous les champs...";
     }
-}
-}
-?>
+}?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -51,26 +55,27 @@ if (isset($_POST['envoi'])) {
     <link rel="stylesheet" href="/public/assets/styles/connection.css"/>
 </head>
 <body>
-    <div class="flex-container">
-        <form method="POST" action="">
-            <div class="logo"><img src="/public/assets/images/logoblanc.png" class="image" alt=""></div>
+    <div class="page-content">
+        <div class="flex-container">
+            <form method="POST" action="">
+                <div class="logo"><img src="/public/assets/images/logoblanc.png" class="image" alt=""></div>
 
-            <label for="username">Nom d'utilisateur:</label>
-            <input class="inputText" id="username" type="text" name="name" autocomplete="off" placeholder="Entrez votre nom d'utilisateur">
+                <label for="username">Nom d'utilisateur:</label>
+                <input class="inputText" id="username" type="text" name="name" autocomplete="off" placeholder="Entrez votre nom d'utilisateur">
 
-            <label for="password">Mot de passe:</label>
-            <input class="inputText" type="password" name="password" autocomplete="off" placeholder="Entrez votre mot de passe">
+                <label for="password">Mot de passe:</label>
+                <input class="inputText" type="password" name="password" autocomplete="off" placeholder="Entrez votre mot de passe">
 
-            <a href="register.php">Créer un compte</a>
-            <input type="submit" name="envoi" id="submitButton">
-            <p id="errorMessage"> <?php echo $message ?></p>
-        </form>
+                <a href="register.php">Créer un compte</a>
+                <input type="submit" name="envoi" id="submitButton">
+            </form>
+        </div>
+        <footer>
+            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">À propos</a>
+            <a href="https://www.univ-amu.fr/">IUT Aix-Marseille</a>
+            <a href="https://github.com/Edouard-Pon/Univ-PHP-Project">Développeurs</a>
+            <a>© 2023 Notre Equipe</a>
+        </footer>
     </div>
-    <footer>
-        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">À propos</a>
-        <a href="https://www.univ-amu.fr/">IUT Aix-Marseille</a>
-        <a href="https://github.com/Edouard-Pon/Univ-PHP-Project">Développeurs</a>
-        <a>© 2023 Notre Equipe</a>
-    </footer>
 </body>
 </html>

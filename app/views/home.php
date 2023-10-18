1fc6b0300fd5ceaeb1ec0b1616ca22738a1efefc
Edouard-Pon <91964969+Edouard-Pon@users.noreply.github.com>
2023-10-18 03:14:23 +0200

    Refactored home view

--- app/views/home.php
+++ app/views/home.php
@@ -4,26 +4,19 @@ class HomeView
     public function show($data, $posts): void
     {
         ob_start();
-        ?>
-        <!--<p>-->
-        <!--    Home Page WIP<br>-->
-        <!--    You are logged in as --><?php //echo $data['name'] ?><!--<br>-->
-        <!--    Email: --><?php //echo $data['email'] ?>
-        <!--    Number: --><?php //echo $data['phone'] ?>
-        <!--    Location: --><?php //echo $data['location'] ?>
-        <!--    Gender: --><?php //echo $data['gender'] ?>
-        <!--    Admin Status: --><?php //echo $data['admin'] ?>
-        <!--    Last connection: --><?php //echo $data['lastco'] ?>
-        <!--</p>-->
-        <div id="right_informations">
-<!--            photo de profil du mec-->
-            <?php echo $data['name']?>
-            <?php include('partials/navbar.php')?>
-        </div>
-        <?php
-        foreach ($posts as $post) {
-            include('posts/post.php');
-        }
+?>
+<?php if (isset($_SESSION['errorMessage'])) { ?>
+<p id="errorMessage"> <?= $_SESSION['errorMessage'] ?></p>
+<?php unset($_SESSION['errorMessage']); } ?>
+<div id="right_informations">
+    <?= $data['name']?>
+    <?php include('partials/navbar.php')?>
+</div>
+<?php
+include('posts/new.php');
+foreach ($posts as $post) {
+include('posts/post.php');
+}
         (new Layout('PasX - Home', ob_get_clean(), 'home'))->show();
     }
 }

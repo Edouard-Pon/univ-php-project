05e80a41ee4b22c8c24c81b01f5a5d74d854a675
LOUARN-Mathis-2225056a <mathis.louarn@etu.univ-amu.fr>
2023-10-20 15:40:38 +0200

    updated error 404 style

--- app/views/Errors.php
+++ app/views/Errors.php
@@ -8,8 +8,20 @@ class ErrorsView
     {
         ob_start();
 ?>
+<style>
+    p {
+        text-align: center;
+        font-size: 30px;
+    }
+    body {
+        display: grid;
+        justify-content: center;
+        align-items: center;
+        min-height: 100vh;
+    }
+</style>
 <p>
-    404 - Page Not Found!
+    404 - Page Not Found! ¯\_(ツ)_/¯
 </p>
 <?php
         (new Layout('PasX - 404 Not Found', ob_get_clean()))->show();

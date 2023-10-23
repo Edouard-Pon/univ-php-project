78c06d324673574f1c97a485a08aa16875af188a
Edouard-Pon <91964969+Edouard-Pon@users.noreply.github.com>
2023-10-23 06:44:31 +0200

    Refactored profile view

--- app/views/Profile.php
+++ app/views/Profile.php
@@ -8,45 +8,39 @@ class ProfileView
     {
         ob_start();
         ?>
+        <div class="category-feed">
+            WIP
+        </div>
+        <div class="feed">
+            <div>
+                <title>User Profile</title>
+                <h1>User Profile</h1>
 
-        <html>
-        <head>
-            <title>User Profile</title>
-        </head>
-        <body>
-        <h1>User Profile</h1>
+                <!-- Display user information here -->
+                <p>Name: <?php echo $data['name']; ?></p>
+                <p>Email: <?php echo $data['email']; ?></p>
+                <p>Phone: <?php echo $data['phone']; ?></p>
+                <p>Location: <?php echo $data['location']; ?></p>
+                <p>Gender: <?php echo $data['gender']; ?></p>
+                <p>Last Login: <?php echo $data['lastco']; ?></p>
 
-        <!-- Display user information here -->
-        <p>Name: <?php echo $data['name']; ?></p>
-        <p>Email: <?php echo $data['email']; ?></p>
-        <p>Phone: <?php echo $data['phone']; ?></p>
-        <p>Location: <?php echo $data['location']; ?></p>
-        <p>Gender: <?php echo $data['gender']; ?></p>
-        <p>Last Login: <?php echo $data['lastco']; ?></p>
-
-        <h2>Profile picture</h2>
-        <img src="<?= $data['profile_picture'] ?>" alt="Image">
-
-        <!-- Provide an option to update user information -->
-        <!-- <a href="update_profile.php">Update Profile</a> -->
-
-        <h3>Your posts</h3>
-        <div id="posts">
+                <h2>Profile picture</h2>
+                <img src="<?= $data['profile_picture'] ?>" alt="Image">
+            </div>
+            <h3>Your posts</h3>
             <?php
             foreach ($posts as $post) {
                 include('posts/post.php');
-                echo "<button>Delete post</button>";
             }
             ?>
         </div>
 
-        <!-- Add a logout link if needed -->
-        <a href="/logout">Logout</a>
-        </body>
-        </html>
-
+        <div class="navbar">
+            <?= $data['name'] ?>
+            <?php include('partials/navbar.php') ?>
+        </div>
         <?php
-        (new Layout('PasX - Profil', ob_get_clean(), 'profil'))->show();
+        (new Layout('PasX - Profil', ob_get_clean(), 'profile'))->show();
     }
 }
 ?>

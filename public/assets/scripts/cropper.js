1cef0525908b0e2f7e8ba38636c0a2ec47871368
Edouard-Pon <91964969+Edouard-Pon@users.noreply.github.com>
2023-10-29 06:00:59 +0100

    Added cropper for profile picture

---
+++ public/assets/scripts/cropper.js
@@ -0,0 +1,75 @@
+const imageInput = document.getElementById('image-input')
+let imageCon, imageConW, imageConH
+function loadImage() {
+    const selectedFile = imageInput.files[0]
+
+    if (selectedFile) {
+        if (selectedFile.type.startsWith('image/')) {
+            const reader = new FileReader()
+
+            reader.onload = function (e) {
+                image.src = e.target.result
+            }
+
+            reader.readAsDataURL(selectedFile)
+        } else {
+            alert("Please select an image file.")
+        }
+    }
+}
+
+const cropBox = document.getElementById('crop-box')
+let cropBoxSize = parseInt(getComputedStyle(cropBox).width)
+
+document.getElementById('zoom-in').addEventListener('click', function () {
+    cropBoxSize = parseInt(getComputedStyle(cropBox).width)
+    cropBox.style.width = (cropBoxSize + 10) + 'px'
+    cropBox.style.height = (cropBoxSize + 10) + 'px'
+})
+
+document.getElementById('zoom-out').addEventListener('click', function () {
+    cropBoxSize = parseInt(getComputedStyle(cropBox).width)
+    cropBox.style.width = (cropBoxSize - 10) + 'px'
+    cropBox.style.height = (cropBoxSize - 10) + 'px'
+})
+
+let isDragging = false
+
+cropBox.addEventListener('mousedown', (e) => {
+    isDragging = true
+    imageCon = document.getElementById('image-con')
+    imageConW = parseInt(imageCon.getBoundingClientRect().width)
+    imageConH = parseInt(imageCon.getBoundingClientRect().height)
+})
+
+document.addEventListener('mousemove', (e) => {
+    if (isDragging) {
+        const xMouse = parseInt(e.clientX)
+        const yMouse = parseInt(e.clientY)
+        const xBox = parseInt(imageCon.getBoundingClientRect().left)
+        const yBox = parseInt(imageCon.getBoundingClientRect().top)
+        const halfBox = cropBoxSize / 2
+        const x = xMouse - xBox - halfBox
+        const y = yMouse - yBox - halfBox
+
+        console.log(`x: ${x}, y: ${y}`)
+        console.log(`x: ${x + cropBoxSize} <= ${imageConW}`)
+        console.log(`y: ${y + cropBoxSize} <= ${imageConH}`)
+
+        document.getElementById('crop-x').value = x;
+        document.getElementById('crop-y').value = y;
+
+        if (x >= 0 && y >= 0 && x + cropBoxSize <= imageConW && y + cropBoxSize <= imageConH) {
+            console.log('Should Move')
+            cropBox.style.left = x + 'px'
+            cropBox.style.top = y + 'px'
+        }
+    }
+})
+
+document.addEventListener('mouseup', () => {
+    isDragging = false
+    document.getElementById('crop-w').value = cropBoxSize;
+    document.getElementById('image-w').value = imageConW;
+    document.getElementById('image-h').value = imageConH;
+})

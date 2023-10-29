ecdb4c58f2958060be4c3a2b023e725dd7275f2b
Edouard-Pon <91964969+Edouard-Pon@users.noreply.github.com>
2023-10-29 06:00:07 +0100

    Refactored code

--- app/views/partials/Navbar.php
+++ app/views/partials/Navbar.php
@@ -11,19 +11,19 @@ class Navbar
 <div id="navbar_self">
     <a href="/home">
         <div>
-            <img src="assets/images/acceuilX.png" alt="Image Acceuil">
+            <img src="/assets/images/acceuilX.png" alt="Image Acceuil">
             <span>Accueil</span>
         </div>
     </a>
     <a href="/explorer">
         <div>
-            <img src="assets/images/explorerX.png" alt="Image Rechercher">
+            <img src="/assets/images/explorerX.png" alt="Image Rechercher">
             <span>Explorer</span>
         </div>
     </a>
     <a href="/profile">
         <div>
-            <img src="assets/images/profilX.png" alt="Image Profil">
+            <img src="/assets/images/profilX.png" alt="Image Profil">
             <span>Profil</span>
         </div>
     </a>

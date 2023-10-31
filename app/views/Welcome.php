b7bbe7b8afa66e8ce2c93bcd5ea3d1cdfcbfff35
LOUARN-Mathis-2225056a <mathis.louarn@etu.univ-amu.fr>
2023-10-31 14:15:15 +0100

    Updated button CSS and html changed a tag to button tag

--- app/views/Welcome.php
+++ app/views/Welcome.php
@@ -15,10 +15,10 @@ class Welcome
     <div class="interface">
         <p class="title">Nous aussi on <br>peut le faire</p>
         <p class="subtitle">Rejoignez dès aujourd'hui</p>
-        <a class="btn signup" href='/signup'>Créer un compte</a>
+        <button class="signup" onclick="location.href = '/signup'">Créer un compte</button>
         <p class="text">En vous inscrivant, vous acceptez les conditions d'utilisation et de confidentialité<br>Politique incluant l'utilisation des cookies.</p>
         <p class="subtitle">Déja un compte?</p>
-        <a class="btn login" href='/login'">Se connecter</a>
+        <button class="login" onclick="location.href = 'login'">Se connecter</button>
     </div>
 </div>
 <?php
